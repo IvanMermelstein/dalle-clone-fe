7fa603437f7bcdb90679aa586c58f0aa46feda3f
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-03-28 15:33:50 -0300

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { FC, ReactNode } from 'react';
+import { logo } from '../public/assets';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link href={'/'}>
+          <Image
+            src={logo}
+            alt='logo'
+            width={100}
+            height={50}
+            className='w-28 object-contain'
+          />
+        </Link>
+        <Link href={'/createPost'}
+          className='font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'
+        >
+          Create
+        </Link>
+      </header>
+      <main
+        className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] h-full min-h-[calc(100vh-73px)]'
+      >
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default Layout;
