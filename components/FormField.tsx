210898593fa8f11c92d28add6fcc65d3d79681c3
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-04-04 17:51:11 -0300

    Deactivate autocomplete on inputs

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -45,6 +45,7 @@ const FormField: FC<FormFieldProps> = ({ labelName, type, name, placeholder, val
         required
         className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 outline-none block w-full p-3'
         disabled={disabled}
+        autoComplete='off'
       />
     </div>
   );
